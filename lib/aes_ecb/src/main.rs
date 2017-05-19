2415849f54932fdc2a828d975ff6511a84e89e7a
Jared Tobin <jared@jtobin.ca>
2017-05-19 20:21:28 +1200

    Use clap.rs.

--- lib/aes_ecb/src/main.rs
+++ lib/aes_ecb/src/main.rs
@@ -1,9 +1,11 @@
 
 extern crate base64;
+extern crate clap;
 extern crate openssl;
 
 use openssl::symm::{Cipher, Crypter, Mode};
 use std::io::{self, Read};
+use clap::{App, Arg};
 
 fn new_crypter_unpadded(
         cipher: Cipher,
@@ -21,9 +23,28 @@ fn new_crypter_unpadded(
     crypter
 }
 
-// FIXME better command line args
-
 fn main() {
+    let args = App::new("aes_ecb")
+                    .version("0.1")
+                    .about("AES ECB/CBC tools")
+                    .arg(Arg::with_name("key")
+                            .short("k")
+                            .long("key")
+                            .value_name("KEY")
+                            .takes_value(true)
+                            .required(true))
+                    .arg(Arg::with_name("mode")
+                            .short("e")
+                            .long("encrypt")
+                            .help("encrypt (instead of decrypt)"))
+                    .arg(Arg::with_name("iv")
+                            .short("i")
+                            .long("iv")
+                            .value_name("INIT")
+                            .takes_value(true)
+                            .help("initial value"))
+                    .get_matches();
+
     let mut buffer = String::new();
 
     io::stdin().read_to_string(&mut buffer).expect("aes_ecb");
@@ -36,9 +57,26 @@ fn main() {
     let decoded_len = decoded.len();
 
     let cipher = Cipher::aes_128_ecb();
-    let mode   = Mode::Decrypt;
-    let key    = b"YELLOW SUBMARINE";
-    let iv     = None;
+
+    let mode = match args.occurrences_of("mode") {
+            0 => Mode::Decrypt,
+            _ => Mode::Encrypt,
+    };
+
+    let key = match args.value_of("key") {
+            Some(text) =>
+                if text.len() == 16 {
+                    text.as_bytes()
+                } else {
+                    panic!("invalid key length!");
+                },
+            None => panic!("no key provided.")
+    };
+
+    let iv = match args.value_of("iv") {
+            Some(text) => Some(text.as_bytes()),
+            None       => None,
+    };
 
     let mut crypter = new_crypter_unpadded(cipher, mode, key, iv);
     let mut result  = vec![0u8; decoded_len + cipher.key_len()];
