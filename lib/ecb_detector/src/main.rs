10cf862cee4e13cf11557802ef8f56342b3e3f1b
Jared Tobin <jared@jtobin.ca>
2017-05-31 21:31:27 +1200

    WIP.

---
+++ lib/ecb_detector/src/main.rs
@@ -0,0 +1,39 @@
+
+extern crate base64;
+
+use std::collections::HashSet;
+use std::io::{self, Read};
+
+fn ecb_detector(encoded: &[u8], size: usize) -> bool {
+    let mut blocks = HashSet::new();
+
+    for block in encoded.chunks(size) {
+        if blocks.contains(block) {
+            return true;
+        }
+
+        blocks.insert(block);
+    }
+
+    false
+}
+
+fn main() {
+    let mut buffer = String::new();
+
+    io::stdin().read_to_string(&mut buffer).expect("ecb_decoder");
+
+    let decoded = match base64::decode(&buffer) {
+        Ok(val)  => val,
+        Err(err) => panic!("{}", err)
+    };
+
+    let ecb = ecb_detector(&decoded[..], 16);
+
+    if ecb {
+        println!("likely ecb");
+    } else {
+        println!("likely cbc");
+    }
+}
+
