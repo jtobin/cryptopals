76497d355b64c76df041b4588830d3f2a207d322
Jared Tobin <jared@jtobin.ca>
2017-08-31 13:55:43 +1200

    Implement PKCS#7 padding in Rust.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod s1c2;
 mod s1c3;
 mod s1c7;
 
+mod s2c9;
 mod s2c10;
 mod s2c11;
 
@@ -12,6 +13,7 @@ fn main() {
     println!("s1c2:\n{}\n", s1c2::s1c2());
     println!("s1c3:\n{}\n", s1c3::s1c3());
     println!("s1c7:\n{}\n", s1c7::s1c7());
+    println!("s2c9:\n{}\n", s2c9::s2c9());
     println!("s2c10:\n{}\n", s2c10::s2c10());
     println!("s2c11:\n{}\n", s2c11::s2c11());
 }
