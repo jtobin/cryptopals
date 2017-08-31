44264eeefe86f0d5ffe8088f9a96c2822b9745f9
Jared Tobin <jared@jtobin.ca>
2017-08-31 13:23:22 +1200

    Updates.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod s1c3;
 mod s1c7;
 
 mod s2c10;
+mod s2c11;
 
 fn main() {
     println!("s1c1:\n{}\n", s1c1::s1c1());
@@ -12,4 +13,5 @@ fn main() {
     println!("s1c3:\n{}\n", s1c3::s1c3());
     println!("s1c7:\n{}\n", s1c7::s1c7());
     println!("s2c10:\n{}\n", s2c10::s2c10());
+    println!("s2c11:\n{}\n", s2c11::s2c11());
 }
